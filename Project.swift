dba9d67d93ca6ed6647907295ef8c80dd97cd4b3
Pedro <pedro@pepicrft.me>
2024-04-16 08:46:55 +0200

    Update Tuist and Mise setup

--- Project.swift
+++ Project.swift
@@ -1,7 +1,7 @@
 import ProjectDescription
 
 let project = Project(name: "Path", targets: [
-    Target(name: "Path", platform: .macOS, product: .staticLibrary, bundleId: "io.tuist.Path", sources: [
+    .target(name: "Path", destinations: .macOS, product: .staticLibrary, bundleId: "io.tuist.Path", sources: [
         "Sources/Path/**/*.swift",
     ]),
 ])
