f2ea1c830c44d1c3a0648082c56de593e3b2d79d
Pedro <pedro@pepicrft.me>
2024-12-30 18:19:05 +0100

    Update Project.swift to change bundle ID and add unit test target for Path

--- Project.swift
+++ Project.swift
@@ -1,7 +1,10 @@
 import ProjectDescription
 
 let project = Project(name: "Path", targets: [
-    .target(name: "Path", destinations: .macOS, product: .staticLibrary, bundleId: "io.tuist.Path", sources: [
+    .target(name: "Path", destinations: .macOS, product: .staticLibrary, bundleId: "dev.tuist.Path", sources: [
         "Sources/Path/**/*.swift",
     ]),
+    .target(name: "PathTests", destinations: .macOS, product: .unitTests, bundleId: "dev.tuist.PathTests", sources: [
+        "Tests/PathTests/**/*.swift"
+    ], dependencies: [.target(name: "Path")])
 ])
